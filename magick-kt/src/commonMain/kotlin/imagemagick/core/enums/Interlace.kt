0b5ce4e250c03346d396c86b46c7675bb7e39ad0
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-23 19:16:19 +0200

    enums: Interlace

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/Interlace.kt
@@ -0,0 +1,23 @@
+package imagemagick.core.enums
+
+/**
+ * Specifies the interlace types.
+ */
+enum class Interlace {
+    /** Undefined */
+    UNDEFINED,
+    /** NoInterlace */
+    NOINTERLACE,
+    /** Line */
+    LINE,
+    /** Plane */
+    PLANE,
+    /** Partition */
+    PARTITION,
+    /** Gif */
+    GIF,
+    /** Jpeg */
+    JPEG,
+    /** Png */
+    Png,
+}
