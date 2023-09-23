a15be489287f0d6c31b8a4572710f5769bbd8d07
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-23 19:12:46 +0200

    enums: CompressionMethod

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/CompressionMethod.kt
@@ -0,0 +1,65 @@
+package imagemagick.core.enums
+
+/**
+ * Specifies compression methods.
+ */
+enum class CompressionMethod {
+    /** Undefined. */
+    UNDEFINED,
+    /** B44A. */
+    B44A,
+    /** B44. */
+    B44,
+    /** BZip. */
+    BZIP,
+    /** DXT1. */
+    DXT1,
+    /** DXT3. */
+    DXT3,
+    /** DXT5. */
+    DXT5,
+    /** Fax. */
+    FAX,
+    /** Group4. */
+    GROUP4,
+    /** JBIG1. */
+    JBIG1,
+    /** JBIG2. */
+    JBIG2,
+    /** JPEG2000. */
+    JPEG2000,
+    /** JPEG. */
+    JPEG,
+    /** LosslessJPEG. */
+    LOSSLESSJPEG,
+    /** LZMA. */
+    LZMA,
+    /** LZW. */
+    LZW,
+    /** NoCompression. */
+    NOCOMPRESSION,
+    /** Piz. */
+    PIZ,
+    /** Pxr24. */
+    PXR24,
+    /** RLE. */
+    RLE,
+    /** Zip. */
+    ZIP,
+    /** ZipS. */
+    ZIPS,
+    /** Zstd. */
+    ZSTD,
+    /** WebP. */
+    WEBP,
+    /** DWAA. */
+    DWAA,
+    /** DWAB. */
+    DWAB,
+    /** BC7. */
+    BC7,
+    /** BC5. */
+    BC5,
+    /** LERC. */
+    LERC,
+}
