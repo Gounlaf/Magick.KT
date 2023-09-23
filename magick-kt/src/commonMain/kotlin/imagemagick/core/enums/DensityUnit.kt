3601b74fc01f0fcf5c1ea7395ec728ede0197e61
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-23 19:12:55 +0200

    enums: DensityUnit

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/DensityUnit.kt
@@ -0,0 +1,15 @@
+package imagemagick.core.enums
+
+/**
+ * Units of image resolution.
+ */
+enum class DensityUnit {
+    /** Undefined. */
+    UNDEFINED,
+
+    /** Pixels per inch. */
+    PIXELS_PER_INCH,
+
+    /** Pixels per centimeter. */
+    PIXELS_PER_CENTIMETER,
+}
