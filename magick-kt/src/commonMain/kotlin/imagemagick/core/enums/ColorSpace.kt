259eb7d0c76c8f933886630603612052a606ec30
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-23 19:12:36 +0200

    enums: ColorSpace

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/ColorSpace.kt
@@ -0,0 +1,77 @@
+package imagemagick.core.enums
+
+/**
+ * Specifies a kind of color space.
+ */
+enum class ColorSpace {
+    /** Undefined. */
+    UNDEFINED,
+    /** CMY. */
+    CMY,
+    /** CMYK. */
+    CMYK,
+    /** Gray. */
+    GRAY,
+    /** HCL. */
+    HCL,
+    /** HCLp. */
+    HCLP,
+    /** HSB. */
+    HSB,
+    /** HSI. */
+    HSI,
+    /** HSL. */
+    HSL,
+    /** HSV. */
+    HSV,
+    /** HWB. */
+    HWB,
+    /** Lab. */
+    LAB,
+    /** LCH. */
+    LCH,
+    /** LCHab. */
+    LCHAB,
+    /** LCHuv. */
+    LCHUV,
+    /** Log. */
+    LOG,
+    /** LMS. */
+    LMS,
+    /** Luv. */
+    LUV,
+    /** OHTA. */
+    OHTA,
+    /** Rec601YCbCr. */
+    REC601YCBCR,
+    /** Rec709YCbCr. */
+    REC709YCBCR,
+    /** RGB. */
+    RGB,
+    /** scRGB. */
+    SCRGB,
+    /** sRGB. */
+    SRGB,
+    /** Transparent. */
+    TRANSPARENT,
+    /** XyY. */
+    XYY,
+    /** XYZ. */
+    XYZ,
+    /** YCbCr. */
+    YCBCR,
+    /** YCC. */
+    YCC,
+    /** YDbDr. */
+    YDBDR,
+    /** YIQ. */
+    YIQ,
+    /** YPbPr. */
+    YPBPR,
+    /** YUV. */
+    YUV,
+    /** LinearGray. */
+    LINEARGRAY,
+    /** Jzazbz. */
+    JZAZBZ,
+}
