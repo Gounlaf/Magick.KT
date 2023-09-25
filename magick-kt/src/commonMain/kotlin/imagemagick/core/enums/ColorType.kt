50d947eb31312555acf3cd5cec8081c88fac8a67
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-25 23:52:53 +0200

    enums: ColorType

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/ColorType.kt
@@ -0,0 +1,35 @@
+package imagemagick.core.enums
+
+//
+// /!\ do not modify order: it's directly linked to the underlying C library enum/order /!\
+//
+
+/**
+ * Specifies the color type of the image.
+ */
+enum class ColorType {
+    /** Undefined. */
+    UNDEFINED,
+    /** Bilevel. */
+    BILEVEL,
+    /** Grayscale. */
+    GRAYSCALE,
+    /** GrayscaleAlpha. */
+    GRAYSCALE_ALPHA,
+    /** Palette. */
+    PALETTE,
+    /** PaletteAlpha. */
+    PALETTE_ALPHA,
+    /** TrueColor. */
+    TRUE_COLOR,
+    /** TrueColorAlpha. */
+    TRUE_COLOR_ALPHA,
+    /** ColorSeparation. */
+    COLOR_SEPARATION,
+    /** ColorSeparationAlpha. */
+    COLOR_SEPARATION_ALPHA,
+    /** Optimize. */
+    OPTIMIZE,
+    /** PaletteBilevelAlpha. */
+    PALETTE_BILEVEL_ALPHA,
+}
