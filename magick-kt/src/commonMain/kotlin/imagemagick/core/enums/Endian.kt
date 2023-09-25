302b717fcc522f02f9f61390fd9012726edbc97c
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-25 23:53:03 +0200

    enums: Endian

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/Endian.kt
@@ -0,0 +1,17 @@
+package imagemagick.core.enums
+
+//
+// /!\ do not modify order: it's directly linked to the underlying C library enum/order /!\
+//
+
+/**
+ * Specifies endian.
+ */
+enum class Endian {
+    /** Undefined. */
+    UNDEFINED,
+    /** LSB. */
+    LSB,
+    /** MSB. */
+    MSB,
+}
