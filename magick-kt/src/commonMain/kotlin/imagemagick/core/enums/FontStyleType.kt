c88e8a53acda896b1d602de222432d53ae57d6ca
Florian Levis <florian.levis@alphanetworks.tv>
2023-09-25 23:53:12 +0200

    enums: FontStyleType

---
+++ magick-kt/src/commonMain/kotlin/imagemagick/core/enums/FontStyleType.kt
@@ -0,0 +1,23 @@
+package imagemagick.core.enums
+
+//
+// /!\ do not modify order: it's directly linked to the underlying C library enum/order /!\
+//
+
+/**
+ * Specifies the style of a font.
+ */
+enum class FontStyleType {
+    /** Undefined. */
+    UNDEFINED,
+    /** Normal. */
+    NORMAL,
+    /** Italic. */
+    ITALIC,
+    /** Oblique. */
+    OBLIQUE,
+    /** Any. */
+    ANY,
+    /** Bold. */
+    BOLD,
+}
